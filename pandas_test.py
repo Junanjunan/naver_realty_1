49268710ff57a0d3b1813606b630233c488320d0
Taltal <taltalrealty@gmail.com>
2022-01-16 00:37:15 +0900

    1/15'

--- pandas_test.py
+++ pandas_test.py
@@ -6,7 +6,9 @@ df = pd.read_excel('매물.xlsx', sheet_name='임대(주택)')
 # print(df['hosil'])
 # print(df['owner_phone'])
 
-a = '서울시 광진구 구의동 221-35'
-b = '구의동 221-35'
+df1 = pd.read_excel('전번매물통합-2022-01-09.xlsx')
+df2 = pd.read_excel('전번매물통합-2022-01-12.xlsx')
 
+df3 = df2.append(df1)
 
+df3.to_excel('합치기.xlsx')
